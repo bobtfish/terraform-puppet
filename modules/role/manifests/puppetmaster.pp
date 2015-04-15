1f7986bdeba50dae961d933f568d6a62d61aed23
Tomas Doran <tdoran@yelp.com>
2015-04-15 01:56:10 +0100

    Fix up apt config

--- modules/role/manifests/puppetmaster.pp
+++ modules/role/manifests/puppetmaster.pp
@@ -1,4 +1,28 @@
 class role::puppetmaster {
-  fail("HI I AM A PUPPET MASTER")
+  class { 'apt':
+    purge_sources_list   => true,
+    purge_sources_list_d => true,
+    purge_preferences_d  => true,
+  }
+  class { 'apt::release':
+    release_id => 'trusty',
+  }
+  apt_key {
+    'puppetlabs':
+      ensure => 'present',
+      id     => '1054B7A24BD6EC30';
+    'docker':
+      ensure => 'present',
+      id => '36A1D7869245C8950F966E92D8576A8BA88D21E9';
+  }
+  apt::source {
+    'puppetlabs':
+      location   => 'http://apt.puppetlabs.com',
+      repos      => 'main';
+    'docker':
+      location => 'https://get.docker.io/ubuntu',
+      repos    => 'main',
+      release  => 'trusty';
+  }
 }
 
